2df62cdcb1a9e3c53bda2712f4e3bbb2d95c2420
Akshat Saxena <103346687+123Akshat@users.noreply.github.com>
2023-11-30 00:50:48 +0530

    first commit

---
+++ src/components/canvas/Ball.jsx
@@ -0,0 +1,9 @@
+import react from 'react'
+
+const Tech = () => {
+  return (
+    <div>Tech</div>
+  )
+}
+
+export default Tech
---
+++ src/constants/index.js
@@ -0,0 +1,262 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "React Native Developer",
+      icon: mobile,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+    {
+      title: "Content Creator",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Design Head",
+      company_name: "IEEE-IAS VIT",
+      
+      iconBg: "#383E56",
+      date: "March 2022 - Present",
+      points: [
+        "Provide visionary guidance for the design team, ensuring alignment with IEEE-IAS Chapter goals.",
+        "Manage and enhance the visual identity, maintaining consistency across all chapter communications.",
+        "Oversee UX/UI design for the chapter's digital platforms, ensuring a user-friendly and visually appealing experience.",
+      ],
+    },
+    {
+      title: "Senior Core Committee Member",
+      company_name: "VIT Film Society Vellore",
+      icon: tesla,
+      iconBg: "#E6DEDD",
+      date: "June 2022 - January 2023",
+      points: [
+        "Provide strategic direction and mentorship within the VIT Film Society's senior core committee.",
+        "Drive seamless planning and execution of film-related events, ensuring engaging and memorable experiences for members.",
+        "Oversee efficient allocation and utilization of resources for film society activities and initiatives.",
+      ],
+    },
+    {
+      title: "Graphic Designer Intern",
+      company_name: "Azeosoft Web Technologies Private Limited",
+      icon: shopify,
+      iconBg: "#383E56",
+      date: "August 2023 - October 2023",
+      points: [
+        "Developing web applications using Figma and other related technologies.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Crafted captivating web pages tailored to the unique needs of diverse clients.",
+      ],
+    },
+    {
+      title: "Software Associate Intern",
+      company_name: "Capgemini",
+      icon: meta,
+      iconBg: "#E6DEDD",
+      date: "August 2023 - November 2023",
+      points: [
+        "Developing and maintaining web applications using Bootstrap and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "actively engage in resolving diverse tickets related to incidents and effectively mitigating issues related to a few UK-based applications.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Car Rent",
+      description:
+        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Job IT",
+      description:
+        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "restapi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "scss",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Trip Guide",
+      description:
+        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      tags: [
+        {
+          name: "nextjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "supabase",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
